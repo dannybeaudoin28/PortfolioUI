73e5e4a2f802b71841a3b5a64b20e5fedd3e605a
Danny B <danny.beaudoin@student.sl.on.ca>
2023-07-04 15:11:10 -0400

    Add sorting capabilities for table

--- src/app/Components/grades/grades.component.ts
+++ src/app/Components/grades/grades.component.ts
@@ -6,15 +6,19 @@ import { GradesService } from 'src/app/Services/grades.service';
   templateUrl: './grades.component.html',
   styleUrls: ['./grades.component.scss'],
 })
-export class GradesComponent implements OnInit{
-  constructor(private gradesService : GradesService) {}
-  
+export class GradesComponent implements OnInit {
+  constructor(private gradesService: GradesService) { }
+
   ngOnInit(): void {
     this.getCourses();
   }
 
   gpa: number = 3.63;
-  courses: any[] = []; 
+  courses: any[] = [];
+  sortNameAsc: boolean = true;
+  sortDescAsc: boolean = true;
+  sortGradeAsc: boolean = true;
+  sortYearAsc: boolean = true;
 
   private getCourses(): void {
     this.gradesService.getAllCourses().subscribe(response => {
@@ -23,4 +27,43 @@ export class GradesComponent implements OnInit{
     });
   }
 
+  //TODO: Add flags, and logic for sorting by description, grade, and year of study
+  protected sortByName(): void {
+    if (this.sortNameAsc === true) {
+      this.courses.sort((a, b) => (a.name > b.name ? -1 : 1));
+    } else {
+      this.courses.sort((a, b) => (a.name < b.name ? -1 : 1));
+    }
+    this.sortNameAsc = !this.sortNameAsc;
+  }
+
+  protected sortByYear(): void {  
+    if (this.sortYearAsc === true) {
+      this.courses.sort((a, b) => (a.year > b.year ? -1 : 1));
+    } else {
+      this.courses.sort((a, b) => (a.year < b.year ? -1 : 1));
+    }
+    this.sortYearAsc = !this.sortYearAsc;
+  }
+
+  protected sortByGrade(): void {
+    if (this.sortGradeAsc === true) {
+      this.courses.sort((a, b) => (a.grade > b.grade ? -1 : 1));
+    } else {
+      this.courses.sort((a, b) => (a.grade < b.grade ? -1 : 1));
+    }
+    this.sortGradeAsc = !this.sortGradeAsc;
+  }
+
+  protected sortByDesc(): void {
+    if (this.sortDescAsc === true) {
+      this.courses.sort((a, b) => (a.desc > b.desc ? -1 : 1));
+    } else {
+      this.courses.sort((a, b) => (a.desc < b.desc ? -1 : 1));
+    }
+    this.sortDescAsc = !this.sortDescAsc;
+  }
+
+  //TODO: Add Search functionality to search courses by identifier
+  //TODO: Make each course clickable
 }
