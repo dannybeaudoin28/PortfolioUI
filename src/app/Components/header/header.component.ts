fc5a895741bbeb296309f6ee71d1d924247de906
dannybeaudoin28 <danny.beaudoin@student.sl.on.ca>
2023-07-23 20:01:51 -0400

    Modify projects component, delete old small nav menu replacing with 3 modern oval buttons

--- src/app/Components/header/header.component.ts
+++ src/app/Components/header/header.component.ts
@@ -7,5 +7,7 @@ import { AboutMeComponent as about} from '../about-me/about-me.component';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-
+  public selectNew() : void {
+    console.log('menu clicked');
+  }
 }
